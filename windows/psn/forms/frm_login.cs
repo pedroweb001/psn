ba1823f8fcffe09ccc0eeef341f099eb90671703
unknown <pedroianbritodasilva@gmail.com>
2023-02-24 16:21:39 -0300

    Projeto psn para windows.

---
+++ windows/psn/forms/frm_login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace psn.forms
+{
+    public partial class frm_login : psn.frm_principal
+    {
+        public frm_login()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
